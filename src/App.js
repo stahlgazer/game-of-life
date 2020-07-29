a4307359aec6a45b23a9e0ca0c415c454d5667b6
Gavin S <stahlgavin@gmail.com>
2020-07-29 11:51:32 -0700

    toggling grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import produce from 'immer';
+import "./App.css";
+
+const numRows = 50;
+const numCols = 50;
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+  console.log(grid);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 10px)`}}>
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div key={`${i}-${k}`}
+          onClick={() => {
+            const newGrid = produce(grid, gridCopy => {
+              gridCopy[i][k] = 1;
+            })
+            setGrid(newGrid)
+          }}
+            style={{
+              width: 10,
+              height: 10,
+              backgroundColor: grid[i][k] ? "purple" : undefined,
+              border: 'solid 1px black'
+            }}
+          />
+        ))
+      )}
     </div>
   );
 }
